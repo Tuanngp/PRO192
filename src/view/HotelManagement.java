ccfbfdf95e6529fed16980b2e6fb5bc67121f197
_.tuanngp._ <nguyengiaphuongtuan@gmail.com>
2023-07-16 15:45:10 +0700

    Update HotelManagement.java

--- src/view/HotelManagement.java
+++ src/view/HotelManagement.java
@@ -3,9 +3,6 @@ package view;
 import model.room.Room;
 
 public class HotelManagement extends Menu<String>{
-    public static void main(String[] args) {
-        
-    }
     static String[] hotelMenu = {"Customer Management", "Room Management", "Order Management", "Menu"};
     private CustomerManagement customerManagement = new CustomerManagement();
     private RoomManagement roomManagement = new RoomManagement();
@@ -14,10 +11,23 @@ public class HotelManagement extends Menu<String>{
     public void execute(String selected) {
         switch(selected) {
             case "1":
-
-            break;
-
+                customerManagement.run();
+                break;
+            case "2":
+                roomManagement.run();
+                break;
+            case "3":
+                orderManagement.run();
+            case "4":
+                break;
+            default:
+                break;
         }
     };
+
+    public static void main(String[] args) {
+        HotelManagement hotelManagement = new HotelManagement();
+        hotelManagement.run();
+    }
 
 }
